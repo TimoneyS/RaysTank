33de79edeb86c611f9a8c80ddfe68fe1b1cc3c67
Ray <Rays1990@163.com>
2019-06-18 17:11:51 +0800

    modify

--- src/com/ray/tank/common/Const.java
+++ src/com/ray/tank/common/Const.java
@@ -4,21 +4,21 @@ import java.awt.Color;
 
 public class Const {
     
-    public static final double  size            = 5;          // �ߴ�
+    public static final double  size            = 5;          // 尺寸
 
-    public static final boolean canPlayerBeKill = false;      // ���̹�˿�������
-    public static final Color   playerColor     = Color.pink; // ���̹����ɫ
+    public static final boolean canPlayerBeKill = false;      // 玩家是否无敌
+    public static final Color   playerColor     = Color.pink; // 玩家颜色
 
-    public static final double  bulletSpeed     = 45;         // �����ӵ��ٶ�
-    public static final double  manmoveSpeed    = 8;          // �ƶ��ٶ�
-    public static final double  botmoveSpeed    = 3;          // �ƶ��ٶ�
+    public static final double  bulletSpeed     = 45;         // 子弹速度
+    public static final double  manmoveSpeed    = 8;          // 玩家移动速度
+    public static final double  botmoveSpeed    = 3;          // AI移动速度
     
-    public static final boolean aiFire          = true;       // ���Է���
-    public static final boolean aiMove          = true;       // �����ƶ�
-    public static final int     aiShootRate     = 100;        // ����ԽСԽ��
+    public static final boolean aiFire          = true;       // AI是否开火
+    public static final boolean aiMove          = false;       // AI是否移动
+    public static final int     aiShootRate     = 100;        // AI射击频率
 
-    public static final int     MaxExplodRadius = 30;         // ��ը���ߴ�
-    public static final int     explodSpeed     = 3;          // ��ը�ĸ����ٶȣ�����Խ��Խ��
+    public static final int     MaxExplodRadius = 30;         // 最大爆炸半径
+    public static final int     explodSpeed     = 3;          // 爆炸扩展速度
 
     public static final int     D_HEIGTH        = 600;
     public static final int     D_WIDTH         = 800;
