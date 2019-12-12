9e7bc502999ecd7cb491541045fec91c77fc41e8
Ray <Rays1990@163.com>
2019-12-13 00:43:55 +0800

    21

--- src/com/ray/tank/main/Game.java
+++ src/com/ray/tank/main/Game.java
@@ -3,7 +3,7 @@ package com.ray.tank.main;
 import com.ray.tank.view.TankFrame;
 
 public class Game {
-	public static void main(String[] args) {
-		new TankFrame().lunch();
-	}
+    public static void main(String[] args) {
+        new TankFrame().lunch();
+    }
 }
