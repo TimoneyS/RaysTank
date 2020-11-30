91abeee2bc0b2a9a3aeeb454ab75f905f0173937
rays1990@163.com <rays1990@163.com>
2020-12-01 00:05:16 +0800

    提交

--- src/com/rays/tank/model/base/CollisionalItem.java
+++
@@ -1,9 +0,0 @@
-package com.rays.tank.model.base;
-
-import com.rays.tank.model.base.crash.Collision;
-
-public abstract class CollisionalItem extends BaseItem {
-    
-    public abstract Collision getCollision();
-    
-}
--- src/com/rays/tank/model/base/HasLife.java
+++
@@ -1,4 +0,0 @@
-package com.rays.tank.model.base;
-
-public interface HasLife {
-}
