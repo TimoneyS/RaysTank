257d58bad35662c46298a0ce206815d3edbff8cd
rays1990@163.com <rays1990@163.com>
2020-12-10 00:28:38 +0800

    提交

--- src/com/rays/tank/controller/BulletControl.java
+++ src/com/rays/tank/controller/BulletControl.java
@@ -11,11 +11,19 @@ public class BulletControl {
         int[] dir = Context.DIRS[bullet.getDirection()];
         int newX = bullet.getX() + dir[0] * bullet.getSpeed();
         int newY = bullet.getY() + dir[1] * bullet.getSpeed();
-        if (newX >= 0 && newX <= Context.D_WIDTH && newY >= 0 && newY <= Context.D_HEIGTH) {
+        int[] rc = Context.toRowAndCol(newX, newY);
+
+        if (newX < 0 || newX > Context.D_WIDTH || newY < 0 || newY > Context.D_HEIGTH) {
+            bullet.destroy();
+        } else if (Context.battleField.getGround()[rc[0]][rc[1]] > 0) {
+            Context.battleField.getGround()[rc[0]][rc[1]] = Context.battleField.getGround()[rc[0]][rc[1]] + 1;
+            if (Context.battleField.getGround()[rc[0]][rc[1]] > 3) {
+                Context.battleField.getGround()[rc[0]][rc[1]]= 0;
+            }
+            bullet.destroy();
+        } else {
             bullet.setX(newX);
             bullet.setY(newY);
-        } else {
-            bullet.destroy();
         }
     }
 }
